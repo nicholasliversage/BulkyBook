c56f13754391bc0071a5dc7bfff66526b32c86a6
nicholasliversage <nicolasliversage@gmail.com>
2021-10-12 17:11:35 +0200

    section5 complete

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {   [Key]
+        public int Id{ get; set; }
+
+        [Required]
+        [Display(Name ="Category Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
